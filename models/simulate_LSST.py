d6ef0829447bb79928559cdd7d991260d53a7678
Yunyi Shen <yshen99@wisc.edu>
2024-12-05 10:52:04 -0500

    simulate LSST

--- models/simulate_LSST.py
+++ models/simulate_LSST.py
@@ -4,23 +4,28 @@ import sys
 import os
 import astropy.units as u
 from astropy.coordinates import SkyCoord
-import rubin_sim.maf.db as db
+#import rubin_sim.maf.db as db
 from scipy.integrate import simpson as simps
 from scipy.interpolate import interp1d
 import numpy as np
 from astropy.cosmology import FlatLambdaCDM
 import sqlite3
+import pysynphot as S
+from cosmolopy import magnitudes, cc
+import matplotlib.pyplot as plt
 
 
-def simulate_lsstLC(sed_surface, spec_time, spec_wavelengths, 
-                    LSSTschedule = "./data/cadence/baseline_v3.3_10yrs.db",
+def simulate_lsstLC(sed_surface, spec_time, # time in days
+                    spec_wavelengths, 
+                    LSSTschedule = "../data/cadence/baseline_v3.3_10yrs.pkl",
                     filters_encoding = {"u": 0, "g": 1, "r": 2, "i": 3, "z": 4, "y": 5},
                     #filters = ["u", "g", "r", "i", "z", "y"], 
-                    filters_loc = "./data/filters/LSST", 
+                    filters_loc = "../data/filters/LSST",
+                    phase_offset_lim = [10,20], # at which day since first observation to peak the transient 
                     len_per_filter = 50,
-                    minimum_LC_size = 10,
+                    minimum_LC_size = 30,
                     max_retry_location = 500):
-    filters = filters_encoding.keys()
+    all_filters = filters_encoding.keys()
     h = 6.626e-27  # Planck's constant (erg·s)
     c = 2.998e10*u.cm/u.s   # Speed of light (cm/s)
     c_AAs = (c).to(u.AA/u.s).value
@@ -33,20 +38,21 @@ def simulate_lsstLC(sed_surface, spec_time, spec_wavelengths,
     gain = 2.2  # Gain in electrons/photon
     fov = 3.5 #degrees on each side
 
-    tmax_d = 200 # max phase of the transient in days
+    tmax_d = 100 # max phase of the transient in days
     tmax_s = tmax_d*86400
 
     cosmo = FlatLambdaCDM(H0=70, Om0=0.3, Tcmb0=2.725)
-    dist_cm = cosmo.luminosity_distance(z=0).to(u.cm).value
+    dist_cm = 10*cc.pc_cm #cosmo.luminosity_distance(z=0).to(u.cm).value
     # Connect to server
-    cnx = sqlite3.connect(LSSTschedule)
+    #cnx = sqlite3.connect(LSSTschedule)
 
     # Get a cursor
-    cur = cnx.cursor()
-
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    df = pd.read_sql_query('SELECT fieldRA, fieldDec, seeingFwhmEff, observationStartMJD, filter, fiveSigmaDepth, skyBrightness, proposalId  FROM observations', cnx)
-    cnx.close()
+    #cur = cnx.cursor()
+    #breakpoint()
+    #cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    #df = pd.read_sql_query('SELECT fieldRA, fieldDec, seeingFwhmEff, observationStartMJD, filter, fiveSigmaDepth, skyBrightness, proposalId  FROM observations', cnx)
+    df = pd.read_pickle(LSSTschedule)
+    #cnx.close()
     # Generate num_points random coordinates
     num_events = 1
 
@@ -72,22 +78,30 @@ def simulate_lsstLC(sed_surface, spec_time, spec_wavelengths,
 
         # Pick a random start time for the transient
         try:
-            start_mjd = np.random.uniform(tmin_obs, tmax_obs/2)
+            start_mjd = np.random.uniform(tmin_obs, (tmax_obs-tmin_obs)/2 + tmin_obs)
         except:
-            start_mjd = tmin_obs
+            start_mjd = tmin_obs + 5
 
-        shifted_times = spec_time/86400 + start_mjd
         # Exclude points beyond max_phase
         df_obs = df_obs[(df_obs['observationStartMJD'] >= start_mjd) & (df_obs['observationStartMJD'] <= (start_mjd+tmax_d))]
         df_obs.sort_values(by=['observationStartMJD'], inplace=True)
-        if len(df_obs) > 0:
+        if len(df_obs) <= minimum_LC_size:
+            continue
+        # decide when the transient peaks
+        peak_offset = np.random.uniform(phase_offset_lim[0], phase_offset_lim[1])
+        new_s = (df_obs['observationStartMJD'].values - np.min(df_obs['observationStartMJD'].values)-peak_offset)*86400
+        
+        # observations
+        df_obs = df_obs[(new_s >= np.min(spec_time)) & (new_s <= np.max(spec_time))]
+        new_s = new_s[(new_s >= np.min(spec_time)) & (new_s <= np.max(spec_time))]
+        if len(df_obs) > minimum_LC_size:
         #resample the SED surface to the times of the observations
-            new_s = (df_obs['observationStartMJD'].values - start_mjd)*86400
+            
             sed_surface = interp1d(spec_time, sed_surface, axis=0, bounds_error=False, fill_value=0)(new_s)
             spec_time = new_s
-
-            # mjd dates of the observations
-            mjd = df_obs['observationStartMJD'].values
+            
+            # phase
+            #phase = df_obs['observationStartMJD'].values
 
             sky_brightness_mags = df_obs['skyBrightness'].values  # Sky brightness, in mag/arcsec^2
             fwhm_eff = df_obs['seeingFwhmEff'].values  # Seeing FWHM in arcsec
@@ -120,8 +134,8 @@ def simulate_lsstLC(sed_surface, spec_time, spec_wavelengths,
                 # Perform the bandpass integrations
                 I1 = simps(y=spec_flux_interp * throughput * wavelengths, x=wavelengths)  # Weighted integral
                 I2 = simps(y=throughput / wavelengths, x=wavelengths)  # Normalization integral
-                fnu = I1 / I2 / c_AAs / (4 * np.pi * dist_cm**2)  # Flux density in erg/s/cm²/Hz
-
+                fnu = I1 / I2 / c_AAs #/ (4 * np.pi * dist_cm**2)  # Flux density in erg/s/cm²/Hz
+                #breakpoint()
                 # Calculate the apparent magnitude
                 with np.errstate(divide='ignore'):
                     mAB = -2.5 * np.log10(fnu) - 48.6  # AB magnitude
@@ -164,26 +178,49 @@ def simulate_lsstLC(sed_surface, spec_time, spec_wavelengths,
             photoband = []
             photoflux = []
             photomask = []
+            phototime = []
             encod = 0
-            for flt in filters:
+            #plt.figure()
+            for flt in all_filters:
                 flt_idx = np.where(filts == flt)[0]
                 len_this_band = len(flt_idx)
                 photoband_tmp = np.zeros(len_per_filter)
                 photoflux_tmp = np.zeros(len_per_filter)
                 phototime_tmp = np.zeros(len_per_filter)
-                photomask_tmp = np.zeros(len_this_band)
+                photomask_tmp = np.zeros(len_per_filter)
                 photoband_tmp[:len_this_band] = encod
                 photoflux_tmp[:len_this_band] = noisy_mags[flt_idx]
-                phototime_tmp[:len_this_band] = mjd[flt_idx] - start_mjd
+                phototime_tmp[:len_this_band] = spec_time[flt_idx]/86400
                 photomask_tmp[:len_this_band] = 1
+
+                #plt.scatter(phototime_tmp, photoflux_tmp, label=flt)
+
+                anyna = np.where(np.logical_not(np.isfinite(photoflux_tmp)))
+                photoflux_tmp[anyna] = 0
+                photomask_tmp[anyna] = 0
+
                 photoband.append(photoband_tmp)
                 photoflux.append(photoflux_tmp)
                 photomask.append(photomask_tmp)
+                phototime.append(phototime_tmp)
                 encod += 1
+            #breakpoint()
             photoband = np.array(photoband).flatten()
             photoflux = np.array(photoflux).flatten()
             phototime = np.array(phototime).flatten()
             photomask = np.array(photomask).flatten()
+
+            '''
+            plt.ylim((-19.5,-16.5))
+            plt.gca().invert_yaxis()
+            plt.legend()
+            plt.xlabel('Time (days)')
+            plt.ylabel('Magnitude')
+            plt.title('Light curves')
+            plt.show()
+            plt.savefig('light_curves.png')
+            breakpoint()
+            '''
             return photoband, photoflux, phototime, photomask
 
 
