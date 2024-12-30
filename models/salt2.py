b9a9734397331d0d5711b56c8016d7ab573240c2
Yunyi Shen <yshen99@wisc.edu>
2024-12-30 12:36:43 -0500

    able to fit salt2 but not generating sed

--- models/salt2.py
+++ models/salt2.py
@@ -1,21 +1,21 @@
 import numpy as np
-from sncosmo.salt2utils import SALT2ColorLaw
 import sncosmo
+from sncosmo.salt2utils import SALT2ColorLaw
 from astropy.table import Table
-from sklearn.model_selection import train_test_split
-from imblearn.pipeline import Pipeline
-from imblearn.over_sampling import RandomOverSampler
-from imblearn.under_sampling import RandomUnderSampler
-from sklearn import svm
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-'''
-m0phase,m0wavelength,m0flux = np.loadtxt('./salt3_template_0.dat',unpack=True)
-m1phase,m1wavelength,m1flux = np.loadtxt('./salt3_template_1.dat',unpack=True)
+# register SALT3 model #
+salt3_0 = os.path.join(script_dir, 'salt3_template_0.dat')
+salt3_1 = os.path.join(script_dir, 'salt3_template_1.dat')
+salt3_color = os.path.join(script_dir, 'salt2_color_correction.dat')
 
-with open('./salt2_color_correction.dat') as fin:
+
+m0phase,m0wavelength,m0flux = np.loadtxt(salt3_0,unpack=True)
+m1phase,m1wavelength,m1flux = np.loadtxt(salt3_1,unpack=True)
+
+with open(salt3_color) as fin:
     lines = fin.readlines()
     for i in range(len(lines)):
         lines[i] = lines[i].replace('\n','')
@@ -24,39 +24,38 @@ with open('./salt2_color_correction.dat') as fin:
     salt2_colormax = float(lines[7].split()[1])
     colorlaw_salt2 = cl = SALT2ColorLaw([salt2_colormin,salt2_colormax],colorlaw_salt2_coeffs)
 
-spectrum = x0*(m0flux + x1*m1flux)*np.exp(-c*cl)
+'''
+source = sncosmo.SALT2Source(m0file=salt3_0,
+                             m1file=salt3_1,
+                             clfile=salt3_color) # this makes it salt3
 '''
 
-# register SALT3 model #
-
-source = sncosmo.SALT2Source(modeldir=None,
-                             m0file='salt3_template_0.dat',
-                             m1file='salt3_template_1.dat',
-                             clfile='salt2_color_correction.dat') # this makes it salt3
-
-
 ## register LSST bandpasses ##
-bands = 'ugrizy'
-for band in bands:
-    source = f"../data/filters/LSST/LSST_LSST.{band}.dat"
-    band = pd.read_csv(source, header=None, sep='\s+', names=['w','t'])
-    bp = sncosmo.Bandpass(band.w.values, band.t.values, name=f'lsst{band}')
-    sncosmo.registry.register(bp)
 
+bands = 'ugrizy'
 bandnames = ['lsst'+band for band in bands]
+
+'''
+for thisband in bands:
+    source = os.path.join(script_dir, f"../data/filters/LSST/LSST_LSST.{thisband}.dat")
+    thisband = pd.read_csv(source, header=None, sep='\s+', names=['w','t'])
+    bp = sncosmo.Bandpass(thisband.w.values, thisband.t.values, name=f'mylsst{thisband}')
+    breakpoint()
+    sncosmo.registry.register(bp)
+'''
+
 ab = sncosmo.get_magsystem('ab')
 
 def makelc(time, mag, band):
+
     lc = Table()
     lc['time'] = time
-    lc['band'] = np.array([bandnames[i] for i in band])
+    lc['band'] = np.array([bandnames[int(i)] for i in band])
     lc['zpsys'] = np.array(['ab']*len(time))
-    lc['zp'] = np.zeros(len(time))
-    lc['flux'] = np.array([ab.mag_to_flux(m, b) for m, b in zip(mag, lc['band'])])
-    lc['fluxerr'] = 1e-8*np.ones(len(time))
+    lc['zp'] = np.zeros(len(time)) + 25
+    lc['flux'] = np.array([ab.band_mag_to_flux(m + 25, b) for m, b in zip(mag, lc['band'])])
+    lc['fluxerr'] = 1e-5*np.ones(len(time))
     return lc
-
-
 
 def fit_supernova(lc):
     """
@@ -74,8 +73,26 @@ def fit_supernova(lc):
         Best-fitting parameters of the model
     """
     bnds = {'t0':(-100,100),'x0':(-1e-3, 1e-3), 'x1':(-3, 3), 'c':(-0.5, 0.5)}
-    mod = sncosmo.Model('salt2-extended')
+    mod = sncosmo.Model('salt3') #sncosmo.Model(source = source)
+    mod.set(z=0.) 
     res = sncosmo.fit_lc(lc, mod, 
                          vparam_names=['t0', 'x0', 'x1', 'c'],
                          bounds=bnds, minsnr=0)
     return res[0].parameters
+
+
+def _salt2sed(params):
+    return params[2]*(m0flux + params[3]*m1flux)*np.exp(-params[4]*cl)
+
+
+
+def getsalt2_sed(time, mag, band, returnparams=False):
+
+    lc = makelc(time, mag, band)
+    params = fit_supernova(lc)
+    breakpoint()
+    sed = _salt2sed(params)
+    if returnparams:
+        return sed, params
+    else:
+        return sed
